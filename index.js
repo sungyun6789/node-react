735ec41515181cf89875338de782f15a8aa5d75c
sungyun <sungyun5423@gmail.com>
2021-04-04 16:02:27 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://sungyun:tjddbs5423@@boilerplate.siowj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    },
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World~ 안녕하세요'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
