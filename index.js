792a1a494da475117a98f0770c3c4c4f1dcc6f76
sungyun <sungyun5423@gmail.com>
2021-04-04 19:09:17 +0900

    bcrypt로 비밀번호 암호화 하기

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ app.post('/register', (req, res) => {
   // 회원가입 할 때 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어줌
 
   const user = new User(req.body);
-
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
