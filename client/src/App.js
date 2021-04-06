e087bc96f98c023de0e6d9429261918347a6fae3
sungyun <sungyun5423@gmail.com>
2021-04-06 23:18:29 +0900

    feat: file directory, add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { Router } from 'express';
+import { Route, Switch } from 'react-router';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
