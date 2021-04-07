447896ef11c2f05ff8aef2ad0e66f0b72a5f45e4
sungyun <sungyun5423@gmail.com>
2021-04-07 23:05:37 +0900

    feat: react-router settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { Switch, Route } from 'react-router';
+import LandingPage from './components/view/LandingPage/LandingPage';
+import LoginPage from './components/view/LoginPage/LoginPage';
+import RegisterPage from './components/view/RegisterPage/RegisterPage';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
