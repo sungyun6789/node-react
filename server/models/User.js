234a94cad8a76aa988033d0e3389bcad5fac39d7
sungyun <sungyun5423@gmail.com>
2021-04-07 22:21:54 +0900

    bug...

--- server/models/User.js
+++ server/models/User.js
@@ -3,8 +3,6 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const jwt = require('jsonwebtoken');
 
-// cb의 의미는 callback의 줄임말이다.
-
 const userSchema = mongoose.Schema({
   name: {
     type: String,
@@ -12,7 +10,7 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
-    trim: true, // 빈칸 없애줌
+    trim: true,
     unique: 1,
   },
   password: {
@@ -38,9 +36,8 @@ const userSchema = mongoose.Schema({
 
 userSchema.pre('save', function (next) {
   var user = this;
-
   if (user.isModified('password')) {
-    // 비밀번호 암호화 시킨다.
+    //비밀번호를 암호화 시킨다.
     bcrypt.genSalt(saltRounds, function (err, salt) {
       if (err) return next(err);
 
@@ -56,7 +53,7 @@ userSchema.pre('save', function (next) {
 });
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
-  // plainPassword 1234567
+  //plainPassword 1234567    암호회된 비밀번호 $2b$10$l492vQ0M4s9YUBfwYkkaZOgWHExahjWC
   bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
     if (err) return cb(err);
     cb(null, isMatch);
@@ -65,9 +62,13 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 
 userSchema.methods.generateToken = function (cb) {
   var user = this;
+  // console.log('user._id', user._id)
 
-  // jsonwebtoken을 이용해서 token 생성하기
+  // jsonwebtoken을 이용해서 token을 생성하기
   var token = jwt.sign(user._id.toHexString(), 'secretToken');
+  // user._id + 'secretToken' = token
+  // ->
+  // 'secretToken' -> user._id
 
   user.token = token;
   user.save(function (err, user) {
@@ -78,11 +79,11 @@ userSchema.methods.generateToken = function (cb) {
 
 userSchema.statics.findByToken = function (token, cb) {
   var user = this;
-
-  // 토큰을 decode 한다
+  // user._id + ''  = token
+  //토큰을 decode 한다.
   jwt.verify(token, 'secretToken', function (err, decoded) {
-    // 유저 아이디를 이용해서 유저를 찾고
-    // 클라이언트에서 가져온 토큰과 디비에 저장된 토큰이 일치하는지 확인
+    //유저 아이디를 이용해서 유저를 찾은 다음에
+    //클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
     user.findOne({ _id: decoded, token: token }, function (err, user) {
       if (err) return cb(err);
       cb(null, user);
