521dd971ece720f33001f1999dd0753e850b2fd5
sungyun <sungyun5423@gmail.com>
2021-04-08 23:39:40 +0900

    redux setting

---
+++ client/src/_reducers/index.js
@@ -0,0 +1,8 @@
+import { combinReudcer } from 'redux';
+// import user from './user_reducer';
+
+const rootReducer = combinReudcer({
+  // user,
+});
+
+export default rootReducer;
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import 'antd/dist/antd';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import rootReducer from './_reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk,
+)(createStore);
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider
+      store={createStoreWithMiddleware(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__(),
+      )}
+    >
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
