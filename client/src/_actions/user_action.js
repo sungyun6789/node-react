d2d24402b4170a909511e790096546a58a1dadbe
sungyun <sungyun5423@gmail.com>
2021-04-11 11:19:31 +0900

    feat: Redux를 사용한 Register 기능 구현

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types';
+import { LOGIN_USER, REGISTER_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = axios
@@ -7,6 +7,16 @@ export function loginUser(dataTosubmit) {
     .then((response) => response.data);
   return {
     type: LOGIN_USER,
+    payload: request,
+  };
+}
+
+export function registerUser(dataTosubmit) {
+  const request = axios
+    .post('/api/users/register', dataTosubmit)
+    .then((response) => response.data);
+  return {
+    type: REGISTER_USER,
     payload: request,
   };
 }
