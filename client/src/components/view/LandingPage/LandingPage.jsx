1abc68280b67f88398ca8461534958a2b5ff8fe9
sungyun <sungyun5423@gmail.com>
2021-04-11 12:39:03 +0900

    feat: Logout

--- client/src/components/view/LandingPage/LandingPage.jsx
+++ client/src/components/view/LandingPage/LandingPage.jsx
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
 
-const LandingPage = () => {
+const LandingPage = (props) => {
   useEffect(() => {
     axios
       .get('api/hello') // get 신호를 보냄
       .then((response) => console.log(response.data)) // 데이터가 있다면 console에 data를 찍고
       .catch((err) => console.log(err)); // 없다면 에러를 찍음
   }, []);
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout').then((response) => {
+      if (response.data.success) {
+        props.history.push('/login');
+        alert('로그아웃 성공');
+      } else {
+        alert('로그아웃 실패');
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -19,6 +31,8 @@ const LandingPage = () => {
       }}
     >
       <h2>시작 페이지</h2>
+
+      <button onClick={onClickHandler}>Logout</button>
     </div>
   );
 };
