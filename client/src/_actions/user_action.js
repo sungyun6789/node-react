5aedb446eebfdf9e1df522b7c2c1e95f55aa5b42
sungyun <sungyun5423@gmail.com>
2021-04-12 06:45:54 +0900

    feat: Authentication Check

--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER, REGISTER_USER } from './types';
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types';
 
 export function loginUser(dataTosubmit) {
   const request = axios
@@ -17,6 +17,16 @@ export function registerUser(dataTosubmit) {
     .then((response) => response.data);
   return {
     type: REGISTER_USER,
+    payload: request,
+  };
+}
+
+export function auth() {
+  const request = axios
+    .get('/api/users/auth')
+    .then((response) => response.data);
+  return {
+    type: AUTH_USER,
     payload: request,
   };
 }
