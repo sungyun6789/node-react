85584e22f0a8aa4622bfb0032308cc4bb5038e5f
sungyun <sungyun5423@gmail.com>
2021-04-14 21:30:31 +0900

    video thumbnails bug...

--- YoutubeClone/client/src/components/App.js
+++ YoutubeClone/client/src/components/App.js
@@ -7,17 +7,12 @@ import LoginPage from './views/LoginPage/LoginPage.js';
 import RegisterPage from './views/RegisterPage/RegisterPage.js';
 import NavBar from './views/NavBar/NavBar';
 import Footer from './views/Footer/Footer';
-import VideoUploadPage from './views/VideoUploadPage/VideoUploadPage';
-
-//null   Anyone Can go inside
-//true   only logged in user can go inside
-//false  logged in user can't go inside
-
+import UploadVideoPage from './views/UploadVideoPage/UploadVideoPage';
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <NavBar />
-      <div style={{ paddingTop: '69px', minHeight: 'calc(100vh - 80px)' }}>
+      <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
@@ -25,7 +20,7 @@ function App() {
           <Route
             exact
             path="/video/upload"
-            component={Auth(VideoUploadPage, true)}
+            component={Auth(UploadVideoPage, true)}
           />
         </Switch>
       </div>
--- YoutubeClone/server/routes/video.js
+++ YoutubeClone/server/routes/video.js
@@ -1,41 +1,77 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require('../models/User');
-
-// const { auth } = require('../middleware/auth');
 const multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
 
-let storage = multer.diskStorage({
+// const { Video } = require('../models/Video');
+// const { Subscriber } = require('../models/Subscriber');
+// const { auth } = require('../middleware/auth');
+
+var storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads/');
+    cb(null, 'uploads/');
   },
   filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     if (ext !== '.mp4') {
-      return cb(res.status(400).end('only mp4 is allowed'), false);
+      return cb(res.status(400).end('only jpg, png, mp4 is allowed'), false);
     }
-    cd(null, true);
+    cb(null, true);
   },
 });
 
-const upload = multer({ storage: storage }).single('file');
+var upload = multer({ storage: storage }).single('file');
 
 //=================================
-//             Video
+//             User
 //=================================
 
 router.post('/uploadfiles', (req, res) => {
-  // 비디오를 서버에 저장한다.
   upload(req, res, (err) => {
     if (err) {
       return res.json({ success: false, err });
     }
     return res.json({
       success: true,
-      url: res.req.file.path,
+      filePath: res.req.file.path,
       fileName: res.req.file.filename,
     });
   });
 });
 
-module.exports = router;
+router.post('/thumbnail', (req, res) => {
+  let thumbsFilePath = '';
+  let fileDuration = '';
+
+  ffmpeg.ffprobe(req.body.filePath, function (err, metadata) {
+    console.dir(metadata);
+    console.log(metadata.format.duration);
+
+    fileDuration = metadata.format.duration;
+  });
+
+  ffmpeg(req.body.filePath)
+    .on('filenames', function (filenames) {
+      console.log('Will generate ' + filenames.join(', '));
+      thumbsFilePath = 'uploads/thumbnails/' + filenames[0];
+    })
+    .on('end', function () {
+      console.log('Screenshots taken');
+      return res.json({
+        success: true,
+        thumbsFilePath: thumbsFilePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .screenshots({
+      // Will take screens at 20%, 40%, 60% and 80% of the video
+      count: 3,
+      folder: 'uploads/thumbnails',
+      size: '320x240',
+      // %b input basename ( filename w/o extension )
+      filename: 'thumbnail-%b.png',
+    });
+});
